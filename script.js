4d3dbd8593b8026da19ce7850c73830cdd38dd15
carloscwb16 <carlos.cardoso22@escola.pr.gov.br>
2024-08-08 08:23:37 -0230

    .

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultados = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+    enunciado: "No âmbito social, você prefere:",
+    alternativas: ["Alternativa 1", "Alternativa 2"]
+},
+{
+    enunciado: "No âmbito ambiental, você prefere:",
+    alternativas: ["Alternativa 3", "Alternativa 4"]
+},
+{
+    enunciado: "No âmbito tecnológico, você prefere:",
+    alternativas: ["Alternativa 5", "Alternativa 6"]
+}
+]
