07cac9a3e3867c25118662a19c09b332af201e17
carloscwb16 <carlos.cardoso22@escola.pr.gov.br>
2024-08-13 08:35:59 -0230

    .

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function mostraPerguntas (){
 function mostraAlternativas (){
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativa = document.createElement("button");
-        botaoAlternativa.textContent = alternativa;
+        botaoAlternativa.textContent = alternativa.texto;
         caixaAlternativas.appendChild(botaoAlternativa);
     }
 }
