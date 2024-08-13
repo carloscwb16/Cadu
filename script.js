d319e7497d028d055fcf79a424d30dcbd088c105
carloscwb16 <carlos.cardoso22@escola.pr.gov.br>
2024-08-13 08:49:47 -0230

    .

--- script.js
+++ script.js
@@ -9,23 +9,46 @@ const perguntas = [
     enunciado: "No âmbito social, você prefere:",
     alternativas: [
         {
-          texto:  "Todas as pessoas que você conhece deixam de falar com você ou amizade para sempre com uma só pessoa", "Você tem o carro mais valioso do mundo mas ninguém da sua família sabe ou você tem o carro mais lento do mundo e todos da sua família sabe"],
-          afirmação:"af",
+          texto:  "Todas as pessoas que você conhece deixam de falar com você ou amizade para sempre com uma só pessoa", 
+         afirmacao:"af",
+        },
+        {
+            texto:"Você tem o carro mais valioso do mundo mas ninguém da sua família sabe ou você tem o carro mais lento do mundo e todos da sua família sabe",
+            afirmacao:"af",
+        },
+    ]
 },
 {
     enunciado: "No âmbito ambiental, você prefere:",
     alternativas: [
-        texto: "Você combate o aquecimento global mas acaba falhando na missão ou você vive em um mundo que o aquecimento global acabou", "Você morre combatendo o desmatamento ou você vive em um mundo sem florestas"],
-        afirmação:"af",
-},
+        {
+            texto: "Você combate o aquecimento global mas acaba falhando na missão ou você vive em um mundo que o aquecimento global acabou", 
+        afirmacao:"af", 
+    },
+    {
+        texto: "Você morre combatendo o desmatamento ou você vive em um mundo sem florestas",
+        afirmacao:"af",
+    }
+    ],
+        },
+        
+
 {
     enunciado: "No âmbito tecnológico, você prefere:",
     alternativas: [
-        texto: "Você constrói o carro mais tecnológico do mundo mas não vai poder usá-lo ou usar um carro problemático", "Usar alguma tecnologia antiga ou não poder usar uma tecnologia atual"],
-        afirmação:"af",
-}
+      {
+          texto: "Você constrói o carro mais tecnológico do mundo mas não vai poder usá-lo ou usar um carro problemático", "Usar alguma tecnologia antiga ou não poder usar uma tecnologia atual"],
+        afirmacao:"af",
+      }
+      {
+        texto: "Usar alguma tecnologia antiga ou não poder usar uma tecnologia atual",
+        afirmacao:"af",
+    },
+],
+      
 
-]
+
+
 
 let atual = 0;
 let perguntaAtual;
